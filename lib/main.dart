513861c62ff9a03877330679d9b85e4bb2875073
Admin1 <mingaleev.d92@mail.ru>
2024-03-11 14:37:35 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: const Scaffold(),
+      home:  Scaffold(
+        appBar: AppBar(),
+      ),
     );
   }
 }
